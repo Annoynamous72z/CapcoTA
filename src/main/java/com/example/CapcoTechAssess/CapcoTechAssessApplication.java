9192b1843b31cdcb9441c1b1eef701391bb1aaf2
Ryan Shi <ryanshi1999@gmail.com>
2023-03-17 16:46:29 -0400

    Added files from Sping Initializer

---
+++ src/main/java/com/example/CapcoTechAssess/CapcoTechAssessApplication.java
@@ -0,0 +1,13 @@
+package com.example.CapcoTechAssess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapcoTechAssessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapcoTechAssessApplication.class, args);
+	}
+
+}
